65affc8e7bee53bfec8a176d0b23bbacde49eaf7
nargespoorhatamii <126357653+nargespoorhatamii@users.noreply.github.com>
2023-04-13 03:45:26 +0430

    Add project files.

---
+++ B.M.M AND K.M.M/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace project
+{
+    class test
+    {
+        public static void Main()
+        {
+            Console.WriteLine("inter two numbers");
+            int a=Convert.ToInt32(Console.ReadLine());
+            int b=Convert.ToInt32(Console.ReadLine());
+           
+          BMM(a,b);
+            KMM(a, b);
+
+        }
+        private  static void BMM(int a, int b)
+        {
+            int n = 0;
+            int m = 0;
+            int e = 0;
+            if (a < b)
+            {
+                m = b;
+                n = a;
+            }
+            if(a>=b)
+            {
+                m = a;
+                n = b;
+            }
+            e= (m % n);
+           while(e>0)
+            {
+                
+                    m = a;
+                    a= e;
+                    e = (m % a);
+
+                
+            }
+          
+            Console.WriteLine("BMM is:" + a);
+
+        }
+        private static void KMM(int x,int y)
+        {
+            int counter = 0;
+            for(int i = 2; i <= x && i<=y;i++)
+                
+                {
+                    if(x%i==0 && y%i==0)
+                {
+                    counter = 1;
+                     
+                }
+                    if (counter==0)
+                        
+                            Console.WriteLine("KMM is:" + (x * y));
+               
+                    
+                else  
+                        { Console.WriteLine("KMM is:" + (x * y) / (x%y)); }
+               
+                    
+                    
+                }
+        }
+    }
+}
